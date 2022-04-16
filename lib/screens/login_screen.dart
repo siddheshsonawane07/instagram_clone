ceb1ca97422c5f1af0f6ca67c20a507091f970c1
Siddhesh Sonawane <siddheshsonawane07@gmail.com>
2022-04-16 13:03:49 +0530

    home screen instagram logo added

---
+++ lib/screens/login_screen.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:instagram_clone/utils/colors.dart';
+
+class LoginScreen extends StatefulWidget {
+  const LoginScreen({Key? key}) : super(key: key);
+
+  @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+          child: Container(
+        padding: const EdgeInsets.symmetric(horizontal: 32),
+        width: double.infinity,
+        child: Column(crossAxisAlignment: CrossAxisAlignment.center, children: [
+          SvgPicture.asset(
+            'assets/logo.svg',
+            color: primaryColor,
+            height: 64,
+          )
+        ]),
+      )),
+    );
+  }
+}
