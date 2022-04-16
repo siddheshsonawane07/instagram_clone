533f9762f8e938c15b55cd5948805c6b6a314c0b
Siddhesh Sonawane <siddheshsonawane07@gmail.com>
2022-04-16 12:52:46 +0530

    firebase setup done for web

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:instagram_clone/responsive/mobile_screen_layout.dart';
 import 'package:instagram_clone/responsive/responsive.dart';
@@ -7,7 +8,19 @@ import 'package:instagram_clone/utils/colors.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  if (kIsWeb) {
+    await Firebase.initializeApp(
+      options: const FirebaseOptions(
+        apiKey: 'AIzaSyDpGnmOQXQsxs1rfYxmw4_tYP_l3GHLbD8',
+        appId: '1:808011769451:web:a502359da9e0c07a06332e',
+        messagingSenderId: '808011769451',
+        projectId: "instagram-fc18e",
+        storageBucket: 'instagram-fc18e.appspot.com',
+      ),
+    );
+  } else {
+    await Firebase.initializeApp();
+  }
   runApp(const MyApp());
 }
 
